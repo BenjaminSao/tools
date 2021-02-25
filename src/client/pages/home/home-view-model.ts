a83a16f6ef3915199be88485d0c9b6e1e5f10d89
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2021-02-25 23:55:51 +0700

    added firestore template and page

--- src/client/pages/home/home-view-model.ts
+++ src/client/pages/home/home-view-model.ts
@@ -1,10 +1,26 @@
-import { PageViewModel, route, template } from "@nivinjoseph/n-app";
+import { NavigationService, PageViewModel, route, template } from "@nivinjoseph/n-app";
+import { inject } from "@nivinjoseph/n-ject";
 import * as Routes from "../routes";
 import "./home-view.scss";
 
 @template(require("./home-view.html"))
 @route(Routes.home)
+@inject("NavigationService")    
 export class HomeViewModel extends PageViewModel
 {
+    private readonly _navigationService: NavigationService;
     
+    
+    public constructor(navigationService: NavigationService)
+    {
+        super();
+        
+        this._navigationService = navigationService;
+    }
+    
+    
+    public gotoFirestore(): void
+    {
+        this._navigationService.navigate(Routes.firestore)
+    }
 }
--- src/client/pages/pages.ts
+++ src/client/pages/pages.ts
@@ -1,5 +1,7 @@
+import { FirestoreViewModel } from "./firestore/firestore-view-model";
 import { HomeViewModel } from "./home/home-view-model";
 
 export const pages: Array<Function> = [
-    HomeViewModel
+    HomeViewModel,
+    FirestoreViewModel
 ];
