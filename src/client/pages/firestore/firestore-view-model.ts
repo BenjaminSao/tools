2d88c868f3708c3eb8c8edaba35720fe4ece5f39
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2021-02-28 22:25:37 +0700

    added some comments

--- src/client/pages/firestore/firestore-view-model.ts
+++ src/client/pages/firestore/firestore-view-model.ts
@@ -9,16 +9,21 @@ import "./firestore-view.scss";
 @inject("FirebaseService")
 export class FirestoreViewModel extends PageViewModel
 {
+    // Reading and writing data from v-model and v-bind
     public get text(): string { return this._text; }
     public set text(value: string) { this._text = value; }
     
+    // v-bind, the data being bind from this._dbData. Exposes data to the view
     public get dbData(): Array<String> { return this._dbData; }
     
+    // Private fields of data
     private _text: string;
     private _dbData: Array<String>;
     
+    // Dependency Injection
     private readonly _firebaseService: FirebaseService;
     
+    // The constructor, ran at the start 
     public constructor(firebaseService: FirebaseService)
     {
         super();
@@ -29,6 +34,7 @@ export class FirestoreViewModel extends PageViewModel
         this._dbData = [];
     }
     
+    // Adding data to the firestore
     public async addData(): Promise<void>
     {
         try
@@ -41,6 +47,7 @@ export class FirestoreViewModel extends PageViewModel
         }
     }
     
+    // Getting Data on mount
     protected onMount(element: HTMLElement): void
     {
         super.onMount(element);
